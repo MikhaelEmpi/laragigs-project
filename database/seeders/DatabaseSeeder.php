8b5a3732656ba1d0f115b3a9fe46ffc790083a5b
MikhaelEmpi <d.mikhaelempi@gmail.com>
2022-12-22 17:28:49 +0700

    feat: :art: Listing Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,46 +17,50 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
 
-        Listing::create([
-                'title' => 'Laravel Senior Developer', 
-                'tags' => 'laravel, javascript',
-                'company' => 'Acme Corp',
-                'location' => 'Boston, MA',
-                'email' => 'email1@email.com',
-                'website' => 'https://www.acme.com',
-                'description' => 'Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Ipsam minima et 
-                illo reprehenderit quas possimus voluptas 
-                repudiandae cum expedita, eveniet aliquid, quam 
-                illum quaerat consequatur! Expedita ab consectetur 
-                tenetur delensiti?'
-            ]
-        );
+        Listing::factory(10)->create();
+        // Listing::create([
+        //         'title' => 'Laravel Senior Developer', 
+        //         'tags' => 'laravel, javascript',
+        //         'company' => 'Acme Corp',
+        //         'location' => 'Boston, MA',
+        //         'email' => 'email1@email.com',
+        //         'website' => 'https://www.acme.com',
+        //         'description' => 'Lorem ipsum dolor sit amet
+        //         consectetur adipisicing elit. Ipsam minima et 
+        //         illo reprehenderit quas possimus voluptas 
+        //         repudiandae cum expedita, eveniet aliquid, quam 
+        //         illum quaerat consequatur! Expedita ab consectetur 
+        //         tenetur delensiti?'
+        //     ]
+        // );
 
-        Listing::create([
-                'title' => 'Backend Developer', 
-                'tags' => 'laravel, php, api',
-                'company' => 'Skynet Systems',
-                'location' => 'Newark, NJ',
-                'email' => 'email4@email.com',
-                'website' => 'https://www.skynet.com',
-                'description' => 'Lorem ipsum dolor sit amet consectetur 
-                adipisicing elit. Ipsam minima et illo reprehenderit quas 
-                possimus voluptas repudiandae cum expedita, eveniet aliquid, 
-                quam illum quaerat consequatur! Expedita ab consectetur tenetur 
-                delensiti?'
-              ]
-        );
+        // Listing::create([
+        //         'title' => 'Backend Developer', 
+        //         'tags' => 'laravel, php, api',
+        //         'company' => 'Skynet Systems',
+        //         'location' => 'Newark, NJ',
+        //         'email' => 'email4@email.com',
+        //         'website' => 'https://www.skynet.com',
+        //         'description' => 'Lorem ipsum dolor sit amet consectetur 
+        //         adipisicing elit. Ipsam minima et illo reprehenderit quas 
+        //         possimus voluptas repudiandae cum expedita, eveniet aliquid, 
+        //         quam illum quaerat consequatur! Expedita ab consectetur tenetur 
+        //         delensiti?'
+        //       ]
+        // );
 
-        Listing::create([
-            'title' => 'Laravel Developer', 
-            'tags' => 'laravel, vue, javascript',
-            'company' => 'Wayne Enterprises',
-            'location' => 'Gotham, NY',
-            'email' => 'email3@email.com',
-            'website' => 'https://www.wayneenterprises.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-          ]);
+        // Listing::create([
+        //     'title' => 'Laravel Developer', 
+        //     'tags' => 'laravel, vue, javascript',
+        //     'company' => 'Wayne Enterprises',
+        //     'location' => 'Gotham, NY',
+        //     'email' => 'email3@email.com',
+        //     'website' => 'https://www.wayneenterprises.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing 
+        //     elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae 
+        //     cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab 
+        //     consectetur tenetur delensiti?'
+        //   ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
