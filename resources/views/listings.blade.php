622108d4b430488a578b370bfc666d804a2fa045
MikhaelEmpi <d.mikhaelempi@gmail.com>
2022-12-23 16:10:35 +0700

    add:laravel theme

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,20 +1,26 @@
+@extends('layout')
+
+@section('content')
+    
 <h1>{{ $heading }}</h1>
 {{-- @if(count($listings) == 0)
     <p>No Listing Found</p>
-@endif 
---}}
-@unless(count($listings) == 0)
-
-@foreach($listings as $listing)
+    @endif 
+    --}}
+    @unless(count($listings) == 0)
+    
+    @foreach($listings as $listing)
     <h2>
         <a href="/listings/{{$listing['id']}}">{{ $listing[ 'title' ] }}</a>
     </h2>
     <p>
         {{ $listing[ 'description' ] }}
     </p>
-@endforeach
-
-@else
+    @endforeach
+    
+    @else
     <p>No Listings Found</p>
-
+    
 @endunless
+
+@endsection
